f789e3f4e98f27a763ffda7cb4a4af86359f8ddc
JoshPayHub <joshuapayumo203@gmail.com>
2025-11-12 22:53:42 +0800

    app balde

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="{{ Vite::asset('resources/assets/dashboard/logo.png') }}">
     @vite('resources/js/app.js')
     @inertiaHead
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
