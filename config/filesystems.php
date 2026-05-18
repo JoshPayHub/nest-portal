567923c506505bfa22f3725f26c9fc7e7085f4b4
JoshPayHub <joshuapayumo203@gmail.com>
2026-05-18 11:11:27 +0800

    feat: public root in filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,9 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path('storage'),
+            'root' => is_dir(base_path('../public_html'))
+                ? base_path('../public_html/storage')
+                : public_path('storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
