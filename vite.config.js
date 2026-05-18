a63e3dd301ed71a7a61f9f4a0984f94a843798af
JoshPayHub <joshuapayumo203@gmail.com>
2026-05-18 12:44:17 +0800

    add

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,22 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
+import path from "path"; // 1. Import the path module
 
 export default defineConfig({
-    base: "/build/", // 🔥 ADD THIS LINE
-
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: [
+                "resources/css/app.css",
+                "resources/js/assets/dashboard/logo.png",
+                "resources/js/app.js",
+            ],
             refresh: true,
         }),
         tailwindcss(),
         vue(),
     ],
-
+    // 2. Add the resolve block to handle the "@" alias
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./resources/js"),
